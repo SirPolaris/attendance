b328d95a926290630913204e71e9e69e174561cf
Justin Kelly <blue_lt@hotmail.com>
2014-05-12 23:00:27 -0400

    Started work on understanding why the code wont excute. Formatted and played with code. Now functioning well. Issue with antiCol. No work on attendance yet.

--- SPITest.py
+++ SPITest.py
@@ -1,5 +1,22 @@
 __author__ = 'Kelly'
 
-import nfc
+#!/usr/bin/python
 
-nfc.readNfc()
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect(host = "localhost", port = 3307, db = "RFID")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print("Database version : %s " % data)
+
+# disconnect from server
+db.close()
